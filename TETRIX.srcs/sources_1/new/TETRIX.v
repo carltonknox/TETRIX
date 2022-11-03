cc5875fb7d9f62d909f11074fbe901c36ba4e089
carltonknox <cknox.bu.edu>
2022-11-03 15:00:15 -0400

    made TETRIX Repo with module and outputs

---
+++ TETRIX.srcs/sources_1/new/TETRIX.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/03/2022 02:53:18 PM
+// Design Name: 
+// Module Name: TETRIX
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TETRIX(
+    input clock,
+    output [199:0] G0,
+    output [199:0] G1,
+    output [199:0] G2,
+    output [199:0] G3
+    );
+endmodule
