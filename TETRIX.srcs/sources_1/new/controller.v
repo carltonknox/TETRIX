b0298f11161073aca465a8e51eeff32244ac97c5
basilnct <57843515+basilnct@users.noreply.github.com>
2022-12-07 17:00:38 -0500

    improve readability

--- TETRIX.srcs/sources_1/new/controller.v
+++ TETRIX.srcs/sources_1/new/controller.v
@@ -38,6 +38,9 @@ module controller(
       1100 = rotate left
     */
     );
+    // player control inputs
+    parameter NO_INPUT = 4'b0000, RIGHT = 4'b0001, LEFT = 4'b1000, INSTA_FALL = 4'b0100, FAST_FALL = 4'b0010, ROTATE_RIGHT = 4'b0011, ROTATE_LEFT = 4'b1100;
+    
     wire [31:0]keycode;
     /*
     P1:
@@ -63,50 +66,50 @@ module controller(
             
 
             // P1
-            8'h15: control0 <= 4'b1100; // rotate left
-            8'h1D: control0 <= 4'b0100; // up
-            8'h24: control0 <= 4'b0011; // rotate right
-            8'h1C: control0 <= 4'b1000; // left
-            8'h1B: control0 <= 4'b0010; // down
-            8'h23: control0 <= 4'b0001; // right
+            8'h15: control0 <= ROTATE_LEFT; // rotate left
+            8'h1D: control0 <= INSTA_FALL; // up
+            8'h24: control0 <= ROTATE_RIGHT; // rotate right
+            8'h1C: control0 <= LEFT; // left
+            8'h1B: control0 <= FAST_FALL; // down
+            8'h23: control0 <= RIGHT; // right
             
             // P2
-            8'h2D: control1 <= 4'b1100;
-            8'h2C: control1 <= 4'b0100;
-            8'h35: control1 <= 4'b0011;
-            8'h2B: control1 <= 4'b1000;
-            8'h34: control1 <= 4'b0010;
-            8'h33: control1 <= 4'b0001;
+            8'h2D: control1 <= ROTATE_LEFT;
+            8'h2C: control1 <= INSTA_FALL;
+            8'h35: control1 <= ROTATE_RIGHT;
+            8'h2B: control1 <= LEFT;
+            8'h34: control1 <= FAST_FALL;
+            8'h33: control1 <= RIGHT;
             
             // P3
-            8'h3C: control2 <= 4'b1100;
-            8'h43: control2 <= 4'b0100;
-            8'h44: control2 <= 4'b0011;
-            8'h3B: control2 <= 4'b1000;
-            8'h42: control2 <= 4'b0010;
-            8'h4B: control2 <= 4'b0001;
+            8'h3C: control2 <= ROTATE_LEFT;
+            8'h43: control2 <= INSTA_FALL;
+            8'h44: control2 <= ROTATE_RIGHT;
+            8'h3B: control2 <= LEFT;
+            8'h42: control2 <= FAST_FALL;
+            8'h4B: control2 <= RIGHT;
             
             // P4
-            8'h22: control3 <= 4'b1100;
-            8'h21: control3 <= 4'b0100;
-            8'h2A: control3 <= 4'b0011;
-            8'h32: control3 <= 4'b1000;
-            8'h31: control3 <= 4'b0010;
-            8'h3A: control3 <= 4'b0001;
+            8'h22: control3 <= ROTATE_LEFT;
+            8'h21: control3 <= INSTA_FALL;
+            8'h2A: control3 <= ROTATE_RIGHT;
+            8'h32: control3 <= LEFT;
+            8'h31: control3 <= FAST_FALL;
+            8'h3A: control3 <= RIGHT;
             
             default: begin 
-                control0 <= 4'b0000;
-                control1 <= 4'b0000;
-                control2 <= 4'b0000;
-                control3 <= 4'b0000;
+                control0 <= NO_INPUT;
+                control1 <= NO_INPUT;
+                control2 <= NO_INPUT;
+                control3 <= NO_INPUT;
             end
         endcase
         end
         else begin
-            control0 <= 4'b0000;
-            control1 <= 4'b0000;
-            control2 <= 4'b0000;
-            control3 <= 4'b0000;
+            control0 <= NO_INPUT;
+            control1 <= NO_INPUT;
+            control2 <= NO_INPUT;
+            control3 <= NO_INPUT;
         end
     end
 endmodule
